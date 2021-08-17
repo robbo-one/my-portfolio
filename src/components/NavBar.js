76c56abe1228800c7d444ca6b96a33820f4318f3
Rob <robsc801@gmail.com>
2021-08-17 13:40:48 +1200

    add social icon links

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+import { SocialIcon } from "react-social-icons";
 
 export default function NavBar() {
   return (
@@ -18,7 +19,7 @@ export default function NavBar() {
             activeClassName="text-white"
             className="inflex-flex items-center py-6 px-3 mr-4 text-red-100 hover:text-green-800 text-4xl font-bold cursive tracking-widest"
           >
-            Rob 
+            Rob
           </NavLink>
           <NavLink
             to="/post"
@@ -27,14 +28,14 @@ export default function NavBar() {
           >
             Blog Posts
           </NavLink>
-        <NavLink
+          <NavLink
             to="/project"
             className="inline-flex items-center py-3 px-3 my-6 rounded text-red-200 hover:text-green-800"
             activeClassName="text-red-100 bg-red-700"
           >
             Projects
           </NavLink>
-        <NavLink
+          <NavLink
             to="/about"
             className="inline-flex items-center py-3 px-3 my-6 rounded text-red-200 hover:text-green-800"
             activeClassName="text-red-100 bg-red-700"
@@ -42,6 +43,29 @@ export default function NavBar() {
             About Me!
           </NavLink>
         </nav>
+        <div>
+          <SocialIcon
+            url="https://github.com/robbo-one"
+            className="mr-4"
+            target="_blank"
+            fgColor="#fff"
+            style={{ height: 35, width: 35 }}
+          />
+          <SocialIcon
+            url="https://www.linkedin.com/in/robbo123/"
+            className="mr-4"
+            target="_blank"
+            fgColor="#fff"
+            style={{ height: 35, width: 35 }}
+          />
+          <SocialIcon
+            url="http://www.im-creator.com/free/robbedphotography/photography"
+            className="mr-4"
+            target="_blank"
+            fgColor="#fff"
+            style={{ height: 35, width: 35 }}
+          />
+        </div>
       </div>
     </header>
   );
