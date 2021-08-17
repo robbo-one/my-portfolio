2a9249a18a831310d7b75cea3b60ebdcb76f8d2c
Rob <robsc801@gmail.com>
2021-08-17 12:52:54 +1200

    navbar working

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,5 +1,29 @@
 import React from  "react"
+import { NavLink }  from "react-router-dom"
+
 
 export default function NavBar() {
-    return <h1>NavBar Page!</h1>
+    return (
+        <header>
+            <div>
+                <nav>
+                    <NavLink to="/" exact>
+                        Rob C
+                    </NavLink>
+
+                    <NavLink to="/post">
+                        Blog Posts
+                    </NavLink>
+
+                    <NavLink to="/project">
+                        Projects
+                    </NavLink>
+                    
+                    <NavLink to="/about">
+                        About Me!
+                    </NavLink>
+                </nav>
+            </div>
+        </header>
+    )
 }
