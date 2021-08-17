dba7273d5b46ba3373fd8bad21947e5f63301318
Rob <robsc801@gmail.com>
2021-08-17 13:14:42 +1200

    add background style to nav menu items on hover

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -18,25 +18,26 @@ export default function NavBar() {
             activeClassName="text-white"
             className="inflex-flex items-center py-6 px-3 mr-4 text-red-100 hover:text-green-800 text-4xl font-bold cursive tracking-widest"
           >
-            Rob C
+            Rob 
           </NavLink>
           <NavLink
             to="/post"
             className="inline-flex items-center py-3 px-3 my-6 rounded text-red-200 hover:text-green-800"
+            activeClassName="text-red-100 bg-red-700"
           >
             Blog Posts
           </NavLink>
-
-          <NavLink
+        <NavLink
             to="/project"
             className="inline-flex items-center py-3 px-3 my-6 rounded text-red-200 hover:text-green-800"
+            activeClassName="text-red-100 bg-red-700"
           >
             Projects
           </NavLink>
-
-          <NavLink
+        <NavLink
             to="/about"
             className="inline-flex items-center py-3 px-3 my-6 rounded text-red-200 hover:text-green-800"
+            activeClassName="text-red-100 bg-red-700"
           >
             About Me!
           </NavLink>
