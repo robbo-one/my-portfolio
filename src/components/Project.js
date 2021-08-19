dd83734b623902d487f987f1ab3584d4823f9aef
Rob <robsc801@gmail.com>
2021-08-19 12:01:10 +1200

    increase view project link

--- src/components/Project.js
+++ src/components/Project.js
@@ -55,7 +55,7 @@ export default function Project() {
                             <p className="my-6 text-lg text-gray-700 leading-relaxed">
                                 {project.description}
                                 </p>
-                                <a href="{project.link}" rel="noopener noreferrer" target="_blank" className="text-red-500 font-bold hover:underline hover:text-red-400"
+                                <a href="{project.link}" rel="noopener noreferrer" target="_blank" className="text-red-500 font-bold hover:underline hover:text-red-400 text-xl"
                                 >
                                 View the Project{" "}
                                 <span role="img" aria-label="right pointer">👉</span>
