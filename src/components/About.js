a0e2ee446aa68eaff4e3a7453a3f308998612ea4
Rob <robsc801@gmail.com>
2021-08-24 16:54:32 +1200

    change background image and style

--- src/components/About.js
+++ src/components/About.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import sanityClient from "../client.js";
 import imageUrlBuilder from "@sanity/image-url";
-import parrot from "../parrot.jpg";
+import turtle from "../turtle.jpg";
 
 const builder = imageUrlBuilder(sanityClient);
 function urlFor(source) {
@@ -28,20 +28,20 @@ export default function About() {
 
   return (
     <main className="relative">
-      <img src={parrot} alt="Parrot" className="absolute w-full" />
-      <div className="p-10  lg:pt-80 container mx-auto relative">
-        <section className="mt-20 bg-green-800 rounded-lg shadow-2xl lg:flex p-10">
+      <img src={turtle} alt="turtle" className="absolute w-full" />
+      <div className="p-20  lg:pt-2 container mx-auto relative">
+        <section className="mt-20 bg-blue-00 rounded-lg shadow-2xl lg:flex p-10">
           <img
             src={urlFor(author.authorImage).url()}
             className="rounded w-32 h-32 lg:w-64 lg:h-64 mr-8"
             alt="Rob"
           />
           <div className="text-lg flex flex-col justify-center">
-            <h1 className="cursive text-6xl text-green-300 mb-4">
+            <h1 className="cursive text-8xl text-black-300 mb-4">
               Hey there. I'm{" "}
-              <span className="text-green-100">{author.name}</span>
+              <span className="text-green-700">{author.name}</span>
             </h1>
-            <p className="text-green-200 text-lg">{author.bio}</p>
+            <p className="text-black-200 text-2xl text-lg">{author.bio}</p>
           </div>
         </section>
       </div>
